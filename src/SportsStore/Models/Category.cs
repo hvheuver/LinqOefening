5f76d1289a51bd0b0787bd3980f383b91891edd0
ksa607 <karine.samyn@hogent.be>
2016-10-10 11:56:33 +0200

    Add starter application

---
+++ src/SportsStore/Models/Category.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace SportsStore.Models
+{
+    public class Category
+    {
+        #region Properties
+        public int CategoryId { get; set; }
+        public string Name { get; set; }
+        public ICollection<Product> Products { get; set; }
+        #endregion
+
+        #region Constructor and Methods
+        public Category()
+        {
+            Products = new List<Product>();
+        }
+
+        public void AddProduct(string naam, decimal price, string description)
+        {
+            if (Products.FirstOrDefault(p => p.Name == naam)==null)
+                Products.Add(new Product(){Description = description, Name = naam, Price = price});
+        }
+
+        public Product FindProduct(string naam)
+        {
+            return Products.FirstOrDefault(p => p.Name == naam);
+        }
+        #endregion
+    }
+}
